7dae697a4aa5f78a50aa65f5e03620f3f59459fc
Jakub Skowroński <j.skowronski39@gmail.com>
2019-12-08 16:02:54 +0100

    Change code formatting

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -5,23 +5,23 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
+    set -- php-fpm "$@"
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
-	if [ "$APP_ENV" != 'prod' ]; then
-		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
-	fi
-	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+    PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
+    if [ "$APP_ENV" != 'prod' ]; then
+        PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+    fi
+    ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
 
-	mkdir -p var/cache var/log
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+    mkdir -p var/cache var/log
+    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
-	if [ "$APP_ENV" != 'prod' ]; then
-		composer install --prefer-dist --no-progress --no-suggest --no-interaction
-	fi
+    if [ "$APP_ENV" != 'prod' ]; then
+        composer install --prefer-dist --no-progress --no-suggest --no-interaction
+    fi
 
 fi
 
