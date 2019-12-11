0304d444b5874d78835f6f0c508446b21f218b0d
Jakub Skowroński <j.skowronski39@gmail.com>
2019-12-12 00:38:15 +0100

    Add user entity

---
+++ src/Entity/User/User.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\User;
+
+class User extends AbstractUser
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
