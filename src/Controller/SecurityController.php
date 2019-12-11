f16d59846f083d56efec501b3cf5f4d370e70ff8
Jakub Skowroński <j.skowronski39@gmail.com>
2019-12-11 21:45:01 +0100

    Add security controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Security\DiscordAuthenticator;
+use App\Security\Enum\ScopeEnum;
+use Exception;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/security", name="app_security")
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/connect/discord", name="_connect_discord")
+     */
+    public function connectAction(ClientRegistry $clientRegistry): RedirectResponse
+    {
+        return $clientRegistry
+            ->getClient('discord_main')
+            ->redirect([
+                ScopeEnum::IDENTIFY,
+            ], [])
+        ;
+    }
+
+    /**
+     * @Route("/connect/discord/check", name="_connect_discord_check")
+     *
+     * @throws Exception
+     *
+     * @see DiscordAuthenticator
+     */
+    public function connectCheckAction(): Response
+    {
+        throw new Exception('This should never be executed!');
+    }
+}
