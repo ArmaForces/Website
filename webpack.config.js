f1eea5c4daef15e623407e8bfd106b00a12ef93d
Filip Maciejewski <veteran29q@gmail.com>
2019-12-20 00:35:36 +0100

    Always enable versioning in webpack assets

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    .enableVersioning(true)
 
     // enables @babel/preset-env polyfills
     .configureBabel(() => {}, {
