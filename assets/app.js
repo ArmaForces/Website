5ae2ef473eda3c3dd90ce5b9cb919aeee4352379
Filip Maciejewski <veteran29q@gmail.com>
2019-12-20 01:19:51 +0100

    Enable bootstrap tooltip plugin, implement custom smooth scroll links

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,21 @@ import './app.scss';
 import jQuery from 'jquery';
 // Init Bootstrap jQuery plugins
 import 'bootstrap';
+
+$(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // smooth scrolling for anchor links
+    $('a.smooth-scroll').on('click', e => {
+        console.log('smooth scrolling', e);
+        e.preventDefault();
+
+        let target = e.target.getAttribute('href');
+        target = `#${target.split('#').pop()}`;
+
+        document.querySelector(target).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+
+});
