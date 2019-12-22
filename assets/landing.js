c81a16e611f291bfd36804cbc6b18b4e535fd57a
Filip Maciejewski <veteran29q@gmail.com>
2019-12-22 22:02:25 +0100

    Not a easter egg

--- assets/landing.js
+++ assets/landing.js
@@ -9,3 +9,16 @@ $(window).on("scroll", () => {
     $('.scrolling-navbar').toggleClass('top-nav-collapse', offset > (window.innerHeight/18));
     $('.scrolling-navbar').toggleClass('bg-dark', offset > (window.innerHeight/8));
 });
+
+$(() => {
+    $('.fa-user-friends').on('click', e => {
+        const el = e.target;
+        let clicks = parseInt(el.dataset.egg || 0);
+        if(clicks >= 5) {
+            $(el).toggleClass(['fa-user-friends', 'fa-wheelchair']);
+            $(el).off('click');
+        } else {
+            el.dataset.egg = clicks + 1
+        }
+    })
+});
