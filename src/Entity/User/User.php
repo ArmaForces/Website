7a49ebdf47bd7d4b77735291d9630179bdd3ee9d
Jakub Skowroński <j.skowronski39@gmail.com>
2019-12-24 17:44:38 +0100

    Add email property

--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -6,8 +6,21 @@ namespace App\Entity\User;
 
 class User extends AbstractUser
 {
+    /** @var string */
+    protected $email;
+
     public function __construct()
     {
         parent::__construct();
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
     }
 }
