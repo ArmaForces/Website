2d123cbe913834f72a91e765b76e572d03fa3a12
Filip Maciejewski <veteran29q@gmail.com>
2020-01-03 02:22:21 +0100

    Add migrations for externalId and avatarHash

---
+++ src/Migrations/Version20200103010805.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200103010805 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add user external_id and avatar_hash';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE users ADD external_id VARCHAR(64) NOT NULL, ADD avatar_hash VARCHAR(512) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE users DROP external_id, DROP avatar_hash');
+    }
+}
