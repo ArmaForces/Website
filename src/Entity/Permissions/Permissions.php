100d47db5e6a5c8b83f7248b59d7df121708fe09
Jakub Skowroński <j.skowronski39@gmail.com>
2020-01-21 23:32:58 +0100

    Add user delete permissions

--- src/Entity/Permissions/Permissions.php
+++ src/Entity/Permissions/Permissions.php
@@ -11,6 +11,9 @@ class Permissions extends AbstractEntity
     /** @var bool */
     protected $managePermissions = false;
 
+    /** @var bool */
+    protected $deleteUsers = false;
+
     public function canManagePermissions(): bool
     {
         return $this->managePermissions;
@@ -19,5 +22,15 @@ class Permissions extends AbstractEntity
     public function setManagePermissions(bool $managePermissions): void
     {
         $this->managePermissions = $managePermissions;
+    }
+
+    public function canDeleteUsers(): bool
+    {
+        return $this->deleteUsers;
+    }
+
+    public function setDeleteUsers(bool $deleteUsers): void
+    {
+        $this->deleteUsers = $deleteUsers;
     }
 }
