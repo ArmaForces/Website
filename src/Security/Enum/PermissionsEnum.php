8e35a67e736c1a37b8aaa34ac5cb3bea6c207142
Jakub Skowroński <j.skowronski39@gmail.com>
2020-01-23 21:00:32 +0100

    Add permissions enum

---
+++ src/Security/Enum/PermissionsEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Enum;
+
+class PermissionsEnum
+{
+    public const MANAGE_USERS_PERMISSIONS = 'MANAGE_USERS_PERMISSIONS';
+    public const DELETE_USERS = 'DELETE_USERS';
+}
