c3ce766a2cea62abd2cfea4a253eed5166fa6a8d
Jakub Skowroński <j.skowronski39@gmail.com>
2020-01-23 21:25:25 +0100

    Add delete users voter

---
+++ src/Security/Voter/User/DeleteUsersVoter.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Voter\User;
+
+use App\Entity\User\UserEntity;
+use App\Security\Enum\PermissionsEnum;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class DeleteUsersVoter extends Voter
+{
+    /**
+     * @param string     $attribute
+     * @param UserEntity $subject
+     */
+    protected function supports($attribute, $subject): bool
+    {
+        return PermissionsEnum::DELETE_USERS === $attribute && $subject instanceof UserInterface;
+    }
+
+    /**
+     * @param string     $attribute
+     * @param UserEntity $subject
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        /** @var UserEntity $user */
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        if ($user->getPermissions()->canDeleteUsers() && $user->getId() !== $subject->getId()) {
+            return true;
+        }
+
+        return false;
+    }
+}
