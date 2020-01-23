471b90e1545bce97ae5e61c42212941398ff3501
Jakub Skowroński <j.skowronski39@gmail.com>
2020-01-23 21:40:37 +0100

    Change enum values to lowercase

--- src/Security/Enum/PermissionsEnum.php
+++ src/Security/Enum/PermissionsEnum.php
@@ -6,7 +6,7 @@ namespace App\Security\Enum;
 
 class PermissionsEnum
 {
-    public const MANAGE_USERS_PERMISSIONS = 'MANAGE_USERS_PERMISSIONS';
-    public const LIST_USERS = 'LIST_USERS';
-    public const DELETE_USERS = 'DELETE_USERS';
+    public const MANAGE_USERS_PERMISSIONS = 'manage_users_permissions';
+    public const LIST_USERS = 'list_users';
+    public const DELETE_USERS = 'delete_users';
 }
