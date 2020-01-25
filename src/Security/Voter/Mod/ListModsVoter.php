70baa9c63734ce8dfe1b24efe066c3a135bc4404
Jakub Skowroński <j.skowronski39@gmail.com>
2020-01-25 23:19:29 +0100

    Add Mods voters

---
+++ src/Security/Voter/Mod/ListModsVoter.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Voter\Mod;
+
+use App\Entity\User\UserEntity;
+use App\Security\Enum\PermissionsEnum;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ListModsVoter extends Voter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject): bool
+    {
+        return PermissionsEnum::MODS_LIST === $attribute;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        /** @var UserEntity $user */
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        if ($user->getPermissions()->getModsPermissions()->canList()) {
+            return true;
+        }
+
+        return false;
+    }
+}
