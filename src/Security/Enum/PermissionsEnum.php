8d0a3e209174d720b6afd851b3a94baa4e7caff5
Jakub Skowroński <j.skowronski39@gmail.com>
2020-01-25 23:19:04 +0100

    Update Permissions enum

--- src/Security/Enum/PermissionsEnum.php
+++ src/Security/Enum/PermissionsEnum.php
@@ -9,4 +9,9 @@ class PermissionsEnum
     public const USERS_MANAGE_PERMISSIONS = 'users_manage_permissions';
     public const USERS_LIST = 'users_list';
     public const USERS_DELETE = 'users_delete';
+
+    public const MODS_LIST = 'mods_list';
+    public const MODS_CREATE = 'mods_create';
+    public const MODS_UPDATE = 'mods_update';
+    public const MODS_DELETE = 'mods_delete';
 }
