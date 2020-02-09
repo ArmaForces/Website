0df2585e5874b2a198d693732c3c628e16f21969
Filip Maciejewski <veteran29q@gmail.com>
2020-02-09 14:43:48 +0100

    WIP

--- assets/discord-widget/components/discordWidget.vue
+++ assets/discord-widget/components/discordWidget.vue
@@ -1,20 +1,43 @@
 <template>
-    <div>
+    <div v-if="error">
+        Error occured during widget load
+    </div>
+    <div v-else>
         {{ url }}
     </div>
 </template>
 
 <script>
   export default {
-      name: "discordWidget",
+      name: "DiscordWidget",
       props: {
           url: {
               required: true,
           },
       },
-      mounted() {
-          console.log('discord widget', this.url);
-      }
+      data() {
+          return {
+              error: false,
+              data: [],
+          };
+      },
+      async mounted() {
+          try {
+              const response = await fetch(this.url);
+              if (!response.ok) {
+                  this.error = true;
+                  return;
+              }
+
+              this.data = await response.json();
+          } catch (e) {
+              this.error = true;
+              throw e;
+          }
+      },
+      methods: {
+          async getGuildData() {}
+      },
   };
 </script>
 
