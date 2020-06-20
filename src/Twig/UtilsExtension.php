747efaff444214f42c8a00b27abb281b09dc08f2
Jakub Skowroński <j.skowronski39@gmail.com>
2020-06-20 15:03:25 +0200

    Improve how default locale is retrieved

--- src/Twig/UtilsExtension.php
+++ src/Twig/UtilsExtension.php
@@ -4,15 +4,32 @@ declare(strict_types=1);
 
 namespace App\Twig;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 use Twig\TwigTest;
 
 class UtilsExtension extends AbstractExtension
 {
+    /** @var ParameterBagInterface */
+    protected $parameterBag;
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->parameterBag = $parameterBag;
+    }
+
     public function getTests(): array
     {
         return [
             new TwigTest('instance_of', [$this, 'instanceOf']),
+        ];
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('get_default_locale', [$this, 'getDefaultLocale']),
         ];
     }
 
@@ -22,5 +39,10 @@ class UtilsExtension extends AbstractExtension
     public function instanceOf($var, string $instance): bool
     {
         return $var instanceof $instance;
+    }
+
+    public function getDefaultLocale(): string
+    {
+        return $this->parameterBag->get('kernel.default_locale');
     }
 }
