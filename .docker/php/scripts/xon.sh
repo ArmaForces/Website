1de436c341e75896b4edc55af0ef4c63484545d3
Filip Maciejewski <veteran29@users.noreply.github.com>
2020-10-18 18:38:27 +0200

    Resolve docker host ip on container startup

--- .docker/php/scripts/xon.sh
+++ .docker/php/scripts/xon.sh
@@ -4,7 +4,6 @@ set -e
 HOST_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
 
 sed -i "s|xdebug.remote_enable=.*|xdebug.remote_enable=1|" $XDEBUG_INI_PATH
-sed -i "s|xdebug.remote_host=.*|xdebug.remote_host=$HOST_IP|" $XDEBUG_INI_PATH
 echo "XDEBUG ON"
 
 pkill -USR2 -o php-fpm
