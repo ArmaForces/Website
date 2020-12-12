a541dea31da9e6b8fe387d2eaec484d62b7a0ee2
Jakub Skowroński <j.skowronski39@gmail.com>
2020-12-12 15:51:38 +0100

    Add Mod Group voters

---
+++ src/Security/Voter/ModGroup/CreateModGroupVoter.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Voter\ModGroup;
+
+use App\Entity\User\UserInterface;
+use App\Security\Enum\PermissionsEnum;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class CreateModGroupVoter extends Voter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject): bool
+    {
+        return PermissionsEnum::MOD_GROUP_CREATE === $attribute;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        /** @var null|UserInterface $currentUser */
+        $currentUser = $token->getUser();
+        if (!$currentUser instanceof UserInterface) {
+            return false;
+        }
+
+        return $currentUser->getPermissions()->getModGroupPermissions()->canCreate();
+    }
+}
