ce37b229b3e9f7e92503d12b9d066ae1183f0421
Jakub Skowroński <j.skowronski39@gmail.com>
2020-12-23 17:16:02 +0100

    Add migration

---
+++ migrations/Version20201223160458.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201223160458 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add Mod List approval';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mod_lists ADD approved TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE permissions ADD mod_list_approve TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mod_lists DROP approved');
+        $this->addSql('ALTER TABLE permissions DROP mod_list_approve');
+    }
+}
