2b0a6f6fdd9fb38f09a7515093d3cef4ad5f492a
Jakub Skowroński <j.skowronski39@gmail.com>
2021-02-06 15:59:55 +0100

    Add basic User Group tests

---
+++ tests/functional/Controller/UserGroupController/CreateActionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Controller\UserGroupController;
+
+use App\DataFixtures\User\AdminUserFixture;
+use App\DataFixtures\User\RegularUserFixture;
+use App\Entity\User\User;
+use App\Test\Enum\RouteEnum;
+use App\Test\Traits\ServicesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @internal
+ * @covers \App\Controller\UserGroupController
+ */
+final class CreateActionTest extends WebTestCase
+{
+    use ServicesTrait;
+
+    /**
+     * @test
+     */
+    public function createAction_anonymousUser_returnsRedirectResponse(): void
+    {
+        $client = $this::getClient();
+        $client->request(Request::METHOD_GET, RouteEnum::USER_GROUP_CREATE);
+
+        $this::assertResponseRedirects(RouteEnum::SECURITY_CONNECT_DISCORD, Response::HTTP_FOUND);
+    }
+
+    /**
+     * @test
+     */
+    public function createAction_unauthorizedUser_returnsForbiddenResponse(): void
+    {
+        /** @var User $user */
+        $user = $this::getEntityById(User::class, RegularUserFixture::ID);
+
+        $client = $this::authenticateClient($user);
+        $client->request(Request::METHOD_GET, RouteEnum::USER_GROUP_CREATE);
+
+        $this::assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @test
+     */
+    public function createAction_authorizedUser_returnsSuccessfulResponse(): void
+    {
+        /** @var User $user */
+        $user = $this::getEntityById(User::class, AdminUserFixture::ID);
+
+        $client = $this::authenticateClient($user);
+        $client->request(Request::METHOD_GET, RouteEnum::USER_GROUP_CREATE);
+
+        $this::assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+}
---
+++ tests/functional/Controller/UserGroupController/DeleteActionTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Controller\UserGroupController;
+
+use App\DataFixtures\User\AdminUserFixture;
+use App\DataFixtures\User\RegularUserFixture;
+use App\Entity\ModGroup\ModGroup;
+use App\Entity\User\User;
+use App\Test\Enum\RouteEnum;
+use App\Test\Traits\DataProvidersTrait;
+use App\Test\Traits\ServicesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @internal
+ * @covers \App\Controller\UserGroupController
+ */
+final class DeleteActionTest extends WebTestCase
+{
+    use ServicesTrait;
+    use DataProvidersTrait;
+
+    /**
+     * @test
+     * @dataProvider modGroupsDataProvider
+     */
+    public function deleteAction_anonymousUser_returnsRedirectResponse(string $modGroupId): void
+    {
+        /** @var ModGroup $subjectModGroup */
+        $subjectModGroup = $this::getEntityById(ModGroup::class, $modGroupId);
+
+        $client = $this::getClient();
+        $client->request(Request::METHOD_GET, sprintf(RouteEnum::MOD_GROUP_DELETE, $subjectModGroup->getName()));
+
+        $this::assertResponseRedirects(RouteEnum::SECURITY_CONNECT_DISCORD, Response::HTTP_FOUND);
+    }
+
+    /**
+     * @test
+     * @dataProvider modGroupsDataProvider
+     */
+    public function deleteAction_unauthorizedUser_returnsForbiddenResponse(string $modGroupId): void
+    {
+        /** @var User $user */
+        $user = $this::getEntityById(User::class, RegularUserFixture::ID);
+
+        /** @var ModGroup $subjectModGroup */
+        $subjectModGroup = $this::getEntityById(ModGroup::class, $modGroupId);
+
+        $client = $this::authenticateClient($user);
+        $client->request(Request::METHOD_GET, sprintf(RouteEnum::MOD_GROUP_DELETE, $subjectModGroup->getName()));
+
+        $this::assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @test
+     * @dataProvider modGroupsDataProvider
+     */
+    public function deleteAction_authorizedUser_returnsSuccessfulResponse(string $modGroupId): void
+    {
+        /** @var User $user */
+        $user = $this::getEntityById(User::class, AdminUserFixture::ID);
+
+        /** @var ModGroup $subjectModGroup */
+        $subjectModGroup = $this::getEntityById(ModGroup::class, $modGroupId);
+
+        $client = $this::authenticateClient($user);
+        $client->request(Request::METHOD_GET, sprintf(RouteEnum::MOD_GROUP_DELETE, $subjectModGroup->getName()));
+
+        $this::assertResponseRedirects(RouteEnum::MOD_GROUP_LIST);
+
+        $client->request(Request::METHOD_GET, sprintf(RouteEnum::MOD_GROUP_DELETE, $subjectModGroup->getName()));
+
+        $this::assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @test
+     */
+    public function deleteAction_authorizedUser_returnsNotFoundResponse(): void
+    {
+        /** @var User $user */
+        $user = $this::getEntityById(User::class, AdminUserFixture::ID);
+
+        $client = $this::authenticateClient($user);
+        $client->request(Request::METHOD_GET, sprintf(RouteEnum::MOD_GROUP_DELETE, 'non-existing-name'));
+
+        $this::assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+}
