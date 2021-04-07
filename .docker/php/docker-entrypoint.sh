e3a39a78cb37893e4c0275ef5f239d1f2b8d3150
Jakub Skowroński <j.skowronski39@gmail.com>
2021-04-07 22:36:01 +0200

    Improve xdebug configuration

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -24,27 +24,4 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
     fi
 fi
 
-# Update xdebug config if exists
-if test -f "$XDEBUG_INI_PATH"; then
-    # https://github.com/qoomon/docker-host/blob/master/entrypoint.sh
-    function resolveHost {
-        getent ahostsv4 "$1" | head -n1 | cut -d' ' -f1
-    }
-    DOCKER_HOST='host.docker.internal'
-    docker_host_ip="$(resolveHost "$DOCKER_HOST")"
-
-    if [ "$docker_host_ip" ]
-    then
-        echo "Docker Host: $docker_host_ip ($DOCKER_HOST)"
-    else
-        docker_host_ip=$(ip -4 route show default | cut -d' ' -f3)
-        if [ "$docker_host_ip" ]
-        then
-            echo "Docker Host: $docker_host_ip (default gateway)"
-        fi
-    fi
-
-    printf '%s\n' 'xdebug.remote_enable=0' "xdebug.remote_host=${docker_host_ip}" 'xdebug.remote_port=9097' 'xdebug.remote_autostart=1' >> $XDEBUG_INI_PATH
-fi
-
 exec docker-php-entrypoint "$@"
