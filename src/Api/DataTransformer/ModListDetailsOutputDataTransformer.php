dfdbb5904973ec78e462275ffeef99fa1461e97d
Jakub Skowroński <j.skowronski39@gmail.com>
2021-06-13 01:48:08 +0200

    Update API

--- src/Api/DataTransformer/ModListDetailsOutputDataTransformer.php
+++ src/Api/DataTransformer/ModListDetailsOutputDataTransformer.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Api\DataTransformer;
 
 use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Api\Dto\DlcOutput;
 use App\Api\Dto\ModListDetailsOutput;
 use App\Api\Dto\ModListOutput;
 use App\Api\Dto\ModOutput;
+use App\Entity\Dlc\DlcInterface;
 use App\Entity\ModList\ModList;
 use App\Entity\ModList\ModListInterface;
 use App\Repository\Mod\ModRepository;
@@ -15,13 +17,16 @@ use App\Repository\Mod\ModRepository;
 class ModListDetailsOutputDataTransformer implements DataTransformerInterface
 {
     protected ModOutputDataTransformer $modDataTransformer;
+    protected DlcOutputDataTransformer $dlcOutputDataTransformer;
     protected ModRepository $modRepository;
 
     public function __construct(
         ModOutputDataTransformer $modDataTransformer,
+        DlcOutputDataTransformer $dlcOutputDataTransformer,
         ModRepository $modRepository
     ) {
         $this->modDataTransformer = $modDataTransformer;
+        $this->dlcOutputDataTransformer = $dlcOutputDataTransformer;
         $this->modRepository = $modRepository;
     }
 
@@ -44,6 +49,12 @@ class ModListDetailsOutputDataTransformer implements DataTransformerInterface
             $mods[] = $this->modDataTransformer->transform($mod, ModOutput::class, $context);
         }
         $output->setMods($mods);
+
+        $dlcs = array_map(
+            fn (DlcInterface $dlc) => $this->dlcOutputDataTransformer->transform($dlc, DlcOutput::class, $context),
+            $modList->getDlcs()
+        );
+        $output->setDlcs($dlcs);
 
         return $output;
     }
--- src/Api/Dto/ModListDetailsOutput.php
+++ src/Api/Dto/ModListDetailsOutput.php
@@ -7,6 +7,7 @@ namespace App\Api\Dto;
 class ModListDetailsOutput extends ModListOutput
 {
     protected array $mods = [];
+    protected array $dlcs = [];
 
     /**
      * @return ModOutput[]
@@ -22,5 +23,21 @@ class ModListDetailsOutput extends ModListOutput
     public function setMods(array $mods): void
     {
         $this->mods = $mods;
+    }
+
+    /**
+     * @return DlcOutput[]
+     */
+    public function getDlcs(): array
+    {
+        return $this->dlcs;
+    }
+
+    /**
+     * @param DlcOutput[] $dlcs
+     */
+    public function setDlcs(array $dlcs): void
+    {
+        $this->dlcs = $dlcs;
     }
 }
