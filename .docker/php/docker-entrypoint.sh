aa551fca0aeca40c7e7ef3051d89872345ebfb68
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-08-14 19:24:27 +0200

    Update entrypoint, disable preload

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -9,15 +9,12 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-    PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
-    if [ "$APP_ENV" != 'prod' ]; then
-        PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
-    fi
-    ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
-    mkdir -p var/cache var/log
-    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var || true
-    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var || true
+    if [ "$APP_ENV" != 'prod' ]; then
+        composer install --prefer-dist --no-progress --no-interaction || true
+    fi
 
     if [ "$APP_ENV" = 'prod' ]; then
         php bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction
