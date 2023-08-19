bc919d0ffba66534b0e2501934ea17930465898d
Jakub Skowroński <j.skowronski39@gmail.com>
2023-08-19 03:43:06 +0200

    Remove invalid typehint

--- src/Repository/UserGroup/UserGroupRepository.php
+++ src/Repository/UserGroup/UserGroupRepository.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace App\Repository\UserGroup;
 
 use App\Entity\UserGroup\UserGroup;
-use App\Entity\UserGroup\UserGroupInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method null|UserGroupInterface find($id, $lockMode = null, $lockVersion = null)
- * @method null|UserGroupInterface findOneBy(array $criteria, array $orderBy = null)
- * @method UserGroupInterface[]    findAll()
- * @method UserGroupInterface[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method null|UserGroup find($id, $lockMode = null, $lockVersion = null)
+ * @method null|UserGroup findOneBy(array $criteria, array $orderBy = null)
+ * @method UserGroup[]    findAll()
+ * @method UserGroup[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class UserGroupRepository extends ServiceEntityRepository
 {
