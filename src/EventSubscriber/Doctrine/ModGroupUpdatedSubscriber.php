afef56dbeeaac2eafb6762f58f5b50fd6f4e625b
Jakub Skowroński <j.skowronski39@gmail.com>
2023-08-26 14:47:34 +0200

    Fix issue with mods lost on update

--- src/EventSubscriber/Doctrine/ModGroupUpdatedSubscriber.php
+++ src/EventSubscriber/Doctrine/ModGroupUpdatedSubscriber.php
@@ -5,21 +5,17 @@ declare(strict_types=1);
 namespace App\EventSubscriber\Doctrine;
 
 use App\Entity\ModGroup\ModGroup;
-use App\Entity\ModList\ModList;
-use App\Entity\User\User;
-use App\Repository\ModList\ModListRepository;
+use App\Service\ModListUpdateService\ModListUpdateServiceInterface;
 use Doctrine\Common\EventSubscriber;
-use Doctrine\ORM\Event\PostFlushEventArgs;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
-use Symfony\Bundle\SecurityBundle\Security;
 
 class ModGroupUpdatedSubscriber implements EventSubscriber
 {
-    private ?ModGroup $updatedModGroup = null;
-
     public function __construct(
-        private Security $security
+        private EntityManagerInterface $entityManager,
+        private ModListUpdateServiceInterface $modListUpdateService,
     ) {
     }
 
@@ -27,48 +23,18 @@ class ModGroupUpdatedSubscriber implements EventSubscriber
     {
         return [
             Events::preUpdate,
-            Events::postFlush,
         ];
     }
 
     public function preUpdate(PreUpdateEventArgs $args): void
     {
-        $entityManager = $args->getObjectManager();
         $modGroup = $args->getObject();
 
         // Do nothing if updated entity is not a Mod Group or no changes were made to the entity
-        if (!$modGroup instanceof ModGroup || !$entityManager->getUnitOfWork()->getEntityChangeSet($modGroup)) {
+        if (!$modGroup instanceof ModGroup || !$this->entityManager->getUnitOfWork()->getEntityChangeSet($modGroup)) {
             return;
         }
 
-        // Save Mod Group for update of associated Mod Lists
-        $this->updatedModGroup = $modGroup;
-    }
-
-    public function postFlush(PostFlushEventArgs $args): void
-    {
-        // Do nothing if no Mod Group updated
-        if (!$this->updatedModGroup) {
-            return;
-        }
-
-        $entityManager = $args->getObjectManager();
-
-        /** @var ModListRepository $modListRepository */
-        $modListRepository = $entityManager->getRepository(ModList::class);
-
-        /** @var null|User $currentUser */
-        $currentUser = $this->security->getUser();
-
-        // Get Mod Lists that use this Mod Group and update their "last changed by/at" properties
-        $modLists = $modListRepository->findModListsContainingModGroup($this->updatedModGroup);
-        foreach ($modLists as $modList) {
-            $modList->updated($currentUser);
-        }
-
-        // Clear updated Mod Group. This prevents executing this method after next flush
-        $this->updatedModGroup = null;
-
-        $entityManager->flush();
+        $this->modListUpdateService->updateModListsAssociatedWithModGroup($modGroup);
     }
 }
---
+++ src/Service/ModListUpdateService/ModListUpdateService.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\ModListUpdateService;
+
+use App\Entity\ModGroup\ModGroup;
+use App\Entity\ModList\ModList;
+use App\Entity\User\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+
+class ModListUpdateService implements ModListUpdateServiceInterface
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private Security $security,
+    ) {
+    }
+
+    public function updateModListsAssociatedWithModGroup(ModGroup $modGroup): void
+    {
+        $currentUser = $this->security->getUser();
+        if (!$currentUser instanceof User) {
+            return;
+        }
+
+        $queryBuilder = $this->entityManager->createQueryBuilder();
+        $expr = $queryBuilder->expr();
+
+        $queryBuilder
+            ->update(ModList::class, 'ml')
+
+            ->set('ml.lastUpdatedAt', ':dateTime')
+            ->setParameter('dateTime', new \DateTimeImmutable())
+
+            ->set('ml.lastUpdatedBy', ':user')
+            ->setParameter('user', $currentUser)
+
+            ->andWhere($expr->isMemberOf(':modGroupId', 'ml.modGroups'))
+            ->setParameter('modGroupId', $modGroup->getId()->toString())
+        ;
+
+        $queryBuilder->getQuery()->execute();
+    }
+}
