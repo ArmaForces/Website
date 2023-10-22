68ceb3c8160beac0a7c0f26b12c2a51c2745e0fe
Jakub Skowroński <j.skowronski39@gmail.com>
2023-10-22 21:00:23 +0200

    Fix migrations

---
+++ migrations/Version20231021222125.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231021222125 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Apply backward compatibility changes';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE mods ALTER status TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE mods ALTER type TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE mods ALTER type SET NOT NULL');
+        $this->addSql('COMMENT ON COLUMN mods.status IS \'(DC2Type:mod_status_enum)\'');
+        $this->addSql('COMMENT ON COLUMN mods.type IS \'(DC2Type:mod_type_enum)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE mods ALTER type TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE mods ALTER type DROP NOT NULL');
+        $this->addSql('ALTER TABLE mods ALTER status TYPE VARCHAR(255)');
+        $this->addSql('COMMENT ON COLUMN mods.type IS NULL');
+        $this->addSql('COMMENT ON COLUMN mods.status IS NULL');
+    }
+}
