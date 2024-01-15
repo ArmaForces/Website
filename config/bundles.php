205f85e25acc558430b035e5df4039a374352aa0
Jakub Skowroński <j.skowronski39@gmail.com>
2024-01-15 14:31:03 +0100

    Remove doctrine test bundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
